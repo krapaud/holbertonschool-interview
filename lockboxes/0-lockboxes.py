11d1d77aa531dcd2449a3ab5ac28b01123ff3892
krapaud <mur.mickael@gmail.com>
2026-04-03 16:44:59 +0200

    Add docstring to canUnlockAll function for clarity

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
 def canUnlockAll(boxes):
+    """Determine if all boxes can be unlocked.
+
+    Each box may contain keys to other boxes. Box 0 is unlocked by default.
+
+    Args:
+        boxes (list of list): A list where boxes[i] contains the keys found in box i.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     elements = [0]
     keys = set()
 
