f4c0f60bd48df0110485dbf54a60c956236ac422
krapaud <mur.mickael@gmail.com>
2026-04-17 09:01:03 +0200

    Implement binary tree node creation and deletion functions

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	malloc(sizeof(binary_tree_t));
+	if (malloc == NULL){
+		return;
+	}
+}
