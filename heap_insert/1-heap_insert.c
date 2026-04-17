2d1e81ef464c2766173ef348ed3ec1551325a605
krapaud <mur.mickael@gmail.com>
2026-04-17 10:11:30 +0200

    Implement heap insertion function and add README for heap_insert module

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+}
